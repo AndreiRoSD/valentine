bf1a47348f65329c5a136e8bc3a8cdaea787951b
AndreiRoSD <78881646+AndreiRoSD@users.noreply.github.com>
2026-01-31 13:52:38 +0200

    Replace GIF with a new image in App component

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ export default function Page() {
     <div className="overflow-hidden flex flex-col items-center justify-center pt-4 h-screen -mt-16 selection:bg-rose-600 selection:text-white text-zinc-900">
       {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <img src="IMG_2429.jpeg" />
           <div className="text-4xl md:text-6xl font-bold my-4">
             Yeeeeeyyyyy muuultumessccc❤️‍🔥❤️‍🔥
           </div>
